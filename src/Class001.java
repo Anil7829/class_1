9639a9f35774092c80398c560c433fbe587c9207
Anil7829 <anilofficial977@gmail.com>
2023-12-18 22:04:19 +0530

    Initial commit

---
+++ src/Class001.java
@@ -0,0 +1,15 @@
+package src;
+
+public class Class001 {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+ int age = 27;
+ String Name = "Anil Sharma";
+ float Height = 6.0f;
+ double Mobile_Number = 09803371244d;
+        System.out.println(Mobile_Number);
+
+
+
+    }
+}
